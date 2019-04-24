1c30c170fc7f49087bbb4d7c6ed3b4526a21a707
Chris Howard <chrisau233@gmail.com>
2019-04-24 15:13:59 +1000

    create api

---
+++ dbConfig.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: "postgres",
+  host: "mydatabase.example.ap-southeast-1.rds.amazonaws.com",
+  database: "mydatabase",
+  password: "mypassword",
+  port: 5432
+};
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Pool } = require("pg");
+
+const dbConfig = require("./dbConfig");
+
+const api = express();
+const pool = new Pool(dbConfig);
+const port = 3000;
+
+// middleware
+api.use(bodyParser.json());
+
+// routes
+api.get("/tasks", (request, response) => {
+  pool.query("SELECT * FROM tasks ORDER BY id DESC", (err, res) => {
+    if (err) return console.log(err);
+    response.json(res.rows);
+  });
+});
+
+api.post("/tasks", (request, response) => {
+  const data = request.body;
+  pool.query(
+    "INSERT INTO tasks(data) VALUES($1)",
+    [JSON.stringify(data)],
+    (err, res) => {
+      if (err) return console.log(err);
+      response.redirect("/tasks");
+    }
+  );
+});
+
+api.put("/tasks/:id", (request, response) => {
+  const { id } = request.params;
+  const data = request.body;
+  pool.query(
+    "UPDATE tasks SET data=($1) WHERE id=($2)",
+    [JSON.stringify(data), id],
+    (err, res) => {
+      if (err) return console.log(err);
+      response.redirect("/tasks");
+    }
+  );
+});
+
+api.delete("/tasks/:id", (request, response) => {
+  const { id } = request.params;
+  pool.query("DELETE FROM tasks WHERE id=($1)", [id], (err, res) => {
+    if (err) return console.log(err);
+    response.redirect("/tasks");
+  });
+});
+
+// start server
+api.listen(port, () => console.log("listening on port: ", port));
